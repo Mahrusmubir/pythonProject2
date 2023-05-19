351aa93de9ffc2a4e4989fb7b4849867ea58f1c0
Mahrus Mubir Chowdhury <95760958+Mahrusmubir@users.noreply.github.com>
2023-05-19 16:50:10 +0600

    Add files via upload

---
+++ pythonProject2/main.py
@@ -0,0 +1,14 @@
+list1 = ["Python", "Java", "CSS", "C#", "C++", "HTML"]
+
+print(list1[-2], list1[-3], list1[-5])
+
+#task 3
+list2 = ["Tom", "Jane", "Henry", "Larry", "Susan"]
+print(list2)
+print(len(list2))
+
+list2[1] = "Tim"
+list2[3] = "Sheldon"
+
+
+print(list2)
